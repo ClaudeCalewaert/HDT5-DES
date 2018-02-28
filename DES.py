688c116e1b2ef5a7c26d377bf8e1ceffc28d0347
ClaudeCalewaert <claudecalewaert86@hotmail.com>
2018-02-27 23:49:13 -0600

    Add files via upload
    Se genero las cantidades aleatorias de memoria e intrucciones

--- DES.py
+++ DES.py
@@ -1,27 +1,30 @@
 import simpy
 import random
 
+
 class SimuladorProcesos:
     def __init__(self, env):
         self.cpu = simpy.Resource(env, capacity=1)
         self.memoria_ram = simpy.Container(env, init=100)
 
 
-def generador_de_procesos(env,cantidad, intervalo_de_procesos, simulador_de_procesos):
+def generador_de_procesos(env, cantidad, intervalo_de_procesos, simulador_de_procesos):
     for i in range(cantidad):
-        p = proceso('Proceso %d' % i, env, simulador_de_procesos)
+        p = proceso('Proceso %d' % i, env, simulador_de_procesos, cantidad_de_memoria=random.randint(1, 10), cantidad_de_instrucciones=random.randint(1, 10))
         env.process(p)
         t = random.expovariate(1.0 / intervalo_de_procesos)
         yield env.timeout(t)
 
 
+def proceso(nombre, env, simulador_de_procesos, cantidad_de_memoria, cantidad_de_instrucciones):
+    print('El %s se encuentra en espera en %d' % (nombre, env.now))
 
-
-
+    yield env.timeout(10) #prueba de yield
 
 env = simpy.Environment()
 simulador_de_procesos = SimuladorProcesos(env)
 semilla_random = 45
 cantidad_de_procesos = 25
 intervalo_de_procesos = 10
-env.process(generador_de_procesos(env, cantidad_de_procesos, intervalo_de_procesos,simulador_de_procesos))
+env.process(generador_de_procesos(env, cantidad_de_procesos, intervalo_de_procesos, simulador_de_procesos))
+env.run()
