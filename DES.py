e3356dcc4ac2c1ee86017907d732ce690a954664
LoboAnimae <30876166+LoboAnimae@users.noreply.github.com>
2018-03-01 20:12:12 -0600

    Added some aesthetics

--- DES.py
+++ DES.py
@@ -2,6 +2,8 @@ import simpy
 import random
 
 
+
+
 class SimuladorProcesos:
     def __init__(self, env):
         self.cpu = simpy.Resource(env, capacity=1)
@@ -18,6 +20,8 @@ def generador_de_procesos(env, cantidad, intervalo_de_procesos, simulador_de_pro
 
 def proceso(nombre, env, simulador_de_procesos, cantidad_de_memoria, cantidad_de_instrucciones):
     print('El %s se encuentra en espera en %d' % (nombre, env.now))
+    print('Cantindad de instrucciones: %d' %(cantidad_de_instrucciones))
+    print('Cantidad de memoria: %d' % (cantidad_de_memoria))
 
    # print('memoria ram %d memoria %d' % (simulador_de_procesos.memoria_ram.level,cantidad_de_memoria)) #prueba de memoria
 
@@ -27,6 +31,7 @@ def proceso(nombre, env, simulador_de_procesos, cantidad_de_memoria, cantidad_de
 
 
 env = simpy.Environment()
+bcs = simpy.Resource(env, capacity)
 simulador_de_procesos = SimuladorProcesos(env)
 semilla_random = 45
 cantidad_de_procesos = 25
